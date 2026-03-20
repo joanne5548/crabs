d6b80f5786047403994de2f3d4daa212ce922d42
joanne5548 <joanne.kim0328@gmail.com>
2026-03-19 21:02:29 -0500

    first commit

---
+++ src/main.rs
@@ -0,0 +1,11 @@
+use catsay-rs::say;
+use std::io::{stdout, BufWriter};
+
+fn main() {
+    let stdout = stdout();
+    let message = String::from("Hellaur!!");
+    let width = message.chars().count();
+
+    let mut writer = BufWriter::new(stdout.lock());
+    say(&message, width, &mut writer).unwrap();
+}
