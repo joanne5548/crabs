c56a451e4b08ae7fd9f796a09730a4aeb399fd19
joanne5548 <joanne.kim0328@gmail.com>
2026-03-22 20:36:49 -0500

    finished temperature converter

--- src/main.rs
+++ src/main.rs
@@ -11,20 +11,27 @@ fn read_line() -> String {
 }
 
 fn main() {
-    println!("Enter temperature in F.");
-
-    let temp_f = read_line();
+    loop {
+        println!("Enter temperature in F.");
     
-    let temp_f: f64 = temp_f
-        .trim()
-        .parse()
-        .expect("Temperature entered was not a number.");
-    
-    let temp_c = (temp_f - 32.0) * 5.0 / 9.0;
-
-    println!("Temperature {temp_f} Fahrenheit is {temp_c:.1} Celsius.");
-    
-    // let mut retry = String::new();
-    // println("Convert another temperature?");
-    // io::stdin()
+        let temp_f = read_line();
+        let temp_f: f64 = temp_f
+            .trim()
+            .parse()
+            .expect("Temperature entered was not a number.");
+        
+        let temp_c = (temp_f - 32.0) * 5.0 / 9.0;
+        println!("Temperature {temp_f} Fahrenheit is {temp_c:.1} Celsius.");
+        
+        println!("Convert another temperature? Enter 1 for yes, 0 for no.");
+        let retry = read_line();
+        let retry: i8 = retry
+            .trim()
+            .parse()
+            .expect("Entered input is not a number.");
+        
+        if retry == 0 {
+            break;
+        }
+    }
 }
